866d19d8983c791cec65c4cf06ce92d0c207de97
braisv <brais.vidal.rodriguez@gmail.com>
2020-03-10 21:09:30 +0100

    computed properties

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = new Vue({
     data: {
         title: 'A New Vueginning',
         background: 'bg-warning',
-        color: false
+        color: false,
+        count: 0
+    },
+    computed: {
+        reverse() {
+            return this.title.split("").reverse().join("")
+        },
+        progress() {
+            return  {
+                'bg-success': this.count <= 10,
+                'bg-warning': this.count > 10 && this.count <= 20,
+                'bg-danger': this.count > 20
+            }
+        }
     }
 })
